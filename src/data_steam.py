71884a40ea2234b359fefed8a987368b2a048f9f
vladbakhteev <bakhteev_v@mail.ru>
2022-09-15 23:30:43 +0200

    read point cloud

--- src/data_steam.py
+++ src/data_steam.py
@@ -1,12 +1,11 @@
 from pathlib import Path
 
-from src.io import read_image, read_json
+from src.io import read_image, read_json, read_ply
 
 
 class DataStream:
     def __init__(self, data_dir):
         self.data_dir = Path(data_dir)
-        pass
 
     def __iter__(self):
         paths = list(self.data_dir.glob('package_*'))
@@ -17,7 +16,7 @@ class DataStream:
         for sample_dir in paths:
             color_frame = read_image(sample_dir / 'color_frame.png')
             meta = read_json(sample_dir / 'meta.txt')
-            point_cloud = None
+            point_cloud = read_ply(sample_dir / 'point_cloud.ply')
 
             sample = {
                 "color_frame": color_frame,
--- src/io.py
+++ src/io.py
@@ -1,13 +1,20 @@
 import json
 
 import cv2
+import open3d as o3d
 
 
 def read_image(p):
     return cv2.imread(str(p))[:, :, ::-1].copy()
+
 
 def read_json(p):
     with open(p) as f:
         data = json.load(f)
     
     return data
+
+
+def read_ply(p):
+    ply = o3d.io.read_point_cloud(str(p))
+    return ply
