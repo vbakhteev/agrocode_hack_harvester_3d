becadee4c0b3bdc6ed7983ca39eb4643198d7ba3
HiGal <f.galeev@inopolis.ru>
2022-09-17 00:09:01 +0300

    reorder steps

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ from src.data_steam import DataStream, OutputStream
 from src.steps import BaseStep, PointsDetection2d, PointsProjection2D
 import matplotlib.pyplot as plt
 
-
 pipeline: List[BaseStep] = [
+    PointsProjection2D(),
     PointsDetection2d(),
-    PointsProjection2D()
+
 ]
 
 
 def main(data_dir: str, output_dir: str):
     istream = DataStream(data_dir)
     ostream = OutputStream(output_dir)
-    
-    for sample in  tqdm(istream, total=len(istream)):
+
+    for sample in tqdm(istream, total=len(istream)):
         for step in pipeline:
             sample = step(sample)
         ostream(sample)
@@ -26,7 +26,7 @@ def main(data_dir: str, output_dir: str):
     ostream.close()
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data', type=str, default='data_sample/')
     parser.add_argument('--out', type=str, default='out/')
