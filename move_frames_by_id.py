00d2662375f092faecfeb7827f1bbcc9846c2014
Bogdanov Sergei <s.v.bogdanov@tinkoff.ru>
2022-09-17 15:23:47 +0200

    auto creation of training data implemented

---
+++ move_frames_by_id.py
@@ -0,0 +1,25 @@
+import json
+from pathlib import Path
+import typing as tp
+
+import cv2
+from tqdm import tqdm
+
+from src.data_steam import DataStream
+
+
+def main():
+    with open("to_label.json") as f:
+        video_to_ids: tp.Dict[str, tp.List[int]] = json.load(f)
+    root_path = Path.cwd() / "frames"
+    if not root_path.exists():
+        root_path.mkdir()
+    for video, ids in video_to_ids.items():
+        istream = DataStream("data/" + video)
+        for idx in ids:
+            sample = istream[idx]
+            save_path = f"frames/{video}_{idx}.jpg"
+            cv2.imwrite(save_path, cv2.cvtColor(sample["color_frame"], cv2.COLOR_BGR2RGB))
+
+if __name__ == "__main__":
+    main()
